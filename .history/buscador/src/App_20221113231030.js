958fbc5f097a9ff7d6eb36a73cdd0d1966b71794
Welynton Renan <65006973+WelyntonRenan@users.noreply.github.com>
2022-11-14 00:32:41 -0300

    Finalizando projeto

---
+++ .history/buscador/src/App_20221113231030.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="Container">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/buscador/src/App_20221114001813.js
@@ -0,0 +1,62 @@
+import React, { useReducer } from 'react';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+import api from './services/api';
+
+function App() {
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useReducer({})
+
+  async function search() {
+    //01001000/json/
+
+    if (input === '') {
+      alert("Você precisa informar um cep primeiro")
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert("Ops! Erro ao buscar cep")
+      setInput("");
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite um CEP"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={search}>
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </div>
+
+      <main className='main'>
+
+        <h2>CEP: {cep.cep}</h2>
+
+        <span>Rua:{cep.lougradouro}</span>
+        <span>Complemento: {cep.complemento}</span>
+        <span>{cep.bairro}</span>
+        <span>{cep.localidade} - {cep.uf}</span>
+
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
